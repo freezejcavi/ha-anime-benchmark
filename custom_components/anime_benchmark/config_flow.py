120fe8b27dc6646226d347ee0b6aad789dea622f
freezejcavi <freeze.jc.avi@seznam.cz>
2026-09-23 20:06:21 +0200

    Add Home Assistant integration scaffold

---
+++ custom_components/anime_benchmark/config_flow.py
@@ -0,0 +1,12 @@
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class AnimeBenchmarkConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(title="Anime Benchmark", data={})
