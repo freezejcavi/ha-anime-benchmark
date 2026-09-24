49ffc4a388cc85aebac7798796ae5668e57047f4
freezejcavi <freeze.jc.avi@seznam.cz>
2026-09-24 11:19:09 +0200

    Add tracker queue configuration constants

--- custom_components/anime_benchmark/const.py
+++ custom_components/anime_benchmark/const.py
@@ -2,3 +2,11 @@ DOMAIN = "anime_benchmark"
 PLATFORMS = ["sensor", "text", "button"]
 ANILIST_URL = "https://graphql.anilist.co"
 STATIC_URL = "/api/anime_benchmark/static"
+
+DEFAULT_TRACKER_REPOSITORY = "freezejcavi/anime-series-tracker"
+DEFAULT_TRACKER_BRANCH = "main"
+DEFAULT_PROFILE_ID = "pr_4a017461bfff446d"
+CONF_GITHUB_TOKEN = "github_token"
+CONF_TRACKER_REPOSITORY = "tracker_repository"
+CONF_TRACKER_BRANCH = "tracker_branch"
+CONF_PROFILE_ID = "profile_id"
